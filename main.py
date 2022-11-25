6e84a83dae56ec549a3ebecd450752c6f7a3fec1
Hendra Manudinata <manoedinata@gmail.com>
2022-11-25 09:50:30 +0000

    saweriaWebhook: Rename to `saweriaWebhook`

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from starter_template import create_app
+from saweriaWebhook import create_app
 
 if __name__ == "__main__":
     app = create_app()
--- saweriaWebhook/__init__.py
+++ saweriaWebhook/__init__.py
@@ -4,7 +4,7 @@ def create_app():
     app = Flask(__name__)
 
     # Main routes
-    from starter_template.main_routes import main as main_routes
+    from saweriaWebhook.main_routes import main as main_routes
     app.register_blueprint(main_routes)
 
     return app
